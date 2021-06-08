6ec21b8ddd668c97f2564b162fdd3e853b109e41
itpark-team <46758840+itpark-team@users.noreply.github.com>
2021-06-08 11:18:35 +0300

    Добавьте файлы проекта.

---
+++ UnitTestProjectSmyslov13/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProjectSmyslov13")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProjectSmyslov13")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f9ec138d-c6f0-4c23-ad8b-bb93f91a34ae")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProjectSmyslov13/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using ClassLibrarySmyslov13;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestProjectSmyslov13
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod_SUM_3_5_finish_8()
+        {
+            CalcClass calc = new CalcClass();
+
+            int expected = 8;
+            int actual = calc.Sum(3, 5);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
